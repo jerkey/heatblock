bb1f88369e53c11fdb47fb21651187a53b2ad9ac
jake waters <jerkey@g-cipher.net>
2016-08-01 20:22:09 -0700

    this should work

--- heatblock.ino
+++ heatblock.ino
@@ -1,5 +1,10 @@
-// #define DEBUG // if DEBUG is defined, ethernet is disabled and serial communications happen
-#include <SPI.h>
+// I need that block to heat up to 63 degrees for half an our and then to 93 for 10 min. That's all really.
+
+#define STAGE1TEMP      63 // temperature for first stage
+#define STAGE1TIME      30*60*1000 // minutes times seconds times milliseconds
+#define STAGE2TEMP      93 // temperature for second stage
+#define STAGE2TIME      10*60*1000 // minutes times seconds times milliseconds
+
 #include <OneWire.h>
 #include <Adafruit_NeoPixel.h>
 
@@ -9,11 +14,16 @@
 #define LEDSTRIP_LEDS   10 // how many LEDs on the strip
 #define HEATER_PIN      7 // to turn on heater
 
-#include "DS18S20.h" // reads temperature from the one digital temp sensor
-#include "button.h" // what does this do?
+#define TEMP_VALID_MIN  -10 // below this temperature is considered an invalid reading
+#define TEMP_VALID_MAX  200 // above this temp is considered an invalid reading
+#define NOHEAT         -200 // set celsiusTarget to this if we don't want heat
 
-float celsiusReading = 255; // stores valid value read from temp sensor
-unsigned long time = 0;
+#include "DS18S20.h" // reads temperature from the one digital temp sensor
+
+float celsiusReading; // stores valid value read from temp sensor
+float celsiusTarget = NOHEAT; // target heating temperature
+byte stage = 0; // what stage are we on?
+unsigned long time,stopTime,lastTempReading;
 Adafruit_NeoPixel LEDStrip = Adafruit_NeoPixel(LEDSTRIP_LEDS, LEDSTRIP_PIN, NEO_GRB + NEO_KHZ800);
 
 void setLEDStrip(byte r, byte g, byte b) {
@@ -23,44 +33,93 @@ void setLEDStrip(byte r, byte g, byte b) {
   }
 }
 
+void printTime(unsigned long time) {
+  Serial.print(time/60000); // time since boot in minutes
+  Serial.print(":");
+  Serial.print((time%60000)/1000); // and seconds
+}
+
 void setup() {
   tone(BEEPER_PIN, 800, 1000); // make a beep (non-blocking function)
   pinMode(HEATER_PIN, OUTPUT);
   Serial.begin(57600);
   Serial.println("\nheatblock");
-  if (initTemp()) {
-    Serial.print("DS18B20 temp sensor found, degrees C = ");
-    Serial.println(getTemp());
-    setMeter(getTemp());
+  while(! initTemp()){ // can't do anything without the temperature sensor
+    printTime(millis()); // print time since boot
+    Serial.println("    ERROR: DS18B20 temp sensor NOT found!!!");
+    setLEDStrip(255,0,0); // red
+    delay(500);
+    setLEDStrip(20,0,0); // dim red
+    delay(500);
   }
-  else Serial.println("ERROR: DS18B20 temp sensor NOT found!!!");
+  for (byte i=0; i<8; i++) Serial.print(DS18S20addr[i],HEX); // print address
+  Serial.print("  DS18B20 temp sensor found, degrees C = ");
+  Serial.println(getTemp());
   LEDStrip.begin(); // init LED strip
   setLEDStrip(0,0,255); // set to blue
 }
 
 void loop() {
   time = millis();
-  if (time - updateMeter >= METER_TIME ) {
-    float lastCelsiusReading = celsiusReading;
-    celsiusReading = getTemp();
-    setMeter(celsiusReading); // set the temperature meter
-    byte colorTemp = constrain(((celsiusReading-20)/20)*255, 0, 255); // gradient from 0 at 20C, 255 at 40C
-    // the next line is where we cause water chemistry to affect the color of the LEDs
-    byte waterChemistry = 0; // how nasty is the water chemistry, 0 = clean, 255 = nasty
-    setLEDStrip(colorTemp,waterChemistry,255-colorTemp); // if clean chemistry: blue at or below 20C, red at or above 40C
-    if ((celsiusReading > TEMP_VALID_MIN) && (celsiusReading < TEMP_VALID_MAX)) {
-      lastTempReading = time; // temperature sensor reported a sane value
-    } else if (time - lastTempReading < MAXREADINGAGE) { // if the last reading isn't too old
-      celsiusReading = lastCelsiusReading; // just use the last valid value
-    }
-    updateMeter = time;
-    if (celsiusReading + HYSTERESIS < set_celsius) {  // only turn on heat if HYSTERESIS deg. C colder than target
-      if (!digitalRead(HEATER_PUMP_PIN)) pumpTime = time; // remember when we last turned on
-      digitalWrite(HEATER_PUMP_PIN,HIGH); // turn on pump
-    }
-    if ((celsiusReading > set_celsius) && (time - pumpTime > PUMPMINTIME)) { // if we reach our goal, turn off heater
-      digitalWrite(HEATER_PUMP_PIN,LOW); // turn off pump
-    }
+  switch(stage) {
+    case 0:
+      stage = 1;
+      celsiusTarget = STAGE1TEMP;
+      stopTime = 0; // don't set stoptime until target is reached
+      break;
+    case 1:
+      if (!stopTime) { // if stopTime hasn't been set yet and is 0
+        if (celsiusReading > celsiusTarget) stopTime = time + STAGE1TIME;
+      } else {
+        if (time > stopTime) {
+          stage = 2;
+          celsiusTarget = STAGE2TEMP;
+          stopTime = 0; // don't set stoptime until target is reached
+        }
+      }
+      break;
+    case 2:
+      if (!stopTime) { // if stopTime hasn't been set yet and is 0
+        if (celsiusReading > celsiusTarget) stopTime = time + STAGE2TIME;
+      } else {
+        if (time > stopTime) {
+          stage = 3;
+          celsiusTarget = NOHEAT;
+        }
+      }
+      break;
+    case 3:
+      digitalWrite(HEATER_PIN,LOW); // turn off heater
+      while(true) { // stay here forever
+        printTime(millis());
+        Serial.print("  Cycle Complete at ");
+        printTime(stopTime);
+        Serial.print("  Heater OFF, temp = ");
+        Serial.println(getTemp());
+        setLEDStrip(0,255*(millis()%2000>1000),0); // blinking green LEDs
+        delay(1000);
+      }
   }
+
+  celsiusReading = getTemp();
+  byte colorTemp = constrain(((celsiusReading-25)/93)*255, 0, 255); // gradient from 0 at 25C, 255 at 93C
+  setLEDStrip(colorTemp,0,255-colorTemp); // blue at or below 25C, red at or above 93C
+  printTime(time);
+  if ((celsiusReading > TEMP_VALID_MIN) && (celsiusReading < TEMP_VALID_MAX)) {
+    lastTempReading = time; // temperature sensor reported a sane value
+    if (celsiusReading < celsiusTarget) {
+      digitalWrite(HEATER_PIN,HIGH); // turn on heater
+      Serial.print("  Heater ON   Temperature = ");
+    } else {
+      digitalWrite(HEATER_PIN,LOW); // turn off heater
+      Serial.print("  Heater OFF  Temperature = ");
+    }
+    Serial.println(celsiusReading);
+  } else {
+    digitalWrite(HEATER_PIN,LOW); // turn off heater
+    Serial.print("invalid temperature value ");
+    Serial.println(celsiusReading);
+  }
+  delay(1000);
 }
 
